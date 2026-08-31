e7346b652229f96fa745d143c4259667e5b252cf
vivet <vivet@vivetonline.com>
2026-08-31 17:21:19 +0200

    Updated

--- Svc.Accounts/Data/AccountsDbContextFactory.cs
+++ Svc.Accounts/Data/AccountsDbContextFactory.cs
@@ -4,4 +4,4 @@ using Nano.Data.MySql;
 namespace Svc.Accounts.Data;
 
 /// <inheritdoc />
-public class AccountsDbContextFactory : BaseDbContextFactory<MySqlProvider, AccountsDbContext>;
+public class AccountsDbContextFactory : BaseDbContextFactory<PostgresSqlProvider2, AccountsDbContext>;
