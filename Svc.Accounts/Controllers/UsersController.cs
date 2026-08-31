80f318ac978b85f9a1253f67229affe3489cd1fe
vivet <vivet@vivetonline.com>
2026-08-31 09:05:57 +0200

    Updated

---
+++ Svc.Accounts/Controllers/UsersController.cs
@@ -0,0 +1,86 @@
+﻿using DynamicExpression.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Nano.App.Api.Controllers;
+using Nano.Common.Consts;
+using Nano.Data.Abstractions;
+using Nano.Data.Abstractions.Identity;
+using Nano.Eventing.Abstractions;
+using Svc.Accounts.Models.Criterias;
+using Svc.Accounts.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Svc.Accounts.Controllers;
+
+/// <inheritdoc />
+public class UsersController(ILogger<UsersController> logger, IRepository repository, IIdentityRepository identityRepository)
+    : BaseEntityUserController<User, UserQueryCriteria>(logger, repository, identityRepository)
+{
+    /// <summary>
+    /// Anonymously get a yser by email.
+    /// </summary>
+    /// <param name="emailAddress">The email address.</param>
+    /// <param name="cancellationToken">The token used when request is cancelled.</param>
+    /// <returns>The user.</returns>
+    /// <response code="200">OK.</response>
+    /// <response code="404">Not Found.</response>
+    /// <response code="400">Bad Request.</response>
+    /// <response code="401">Unauthorized.</response>
+    /// <response code="500">Error occurred.</response>
+    [HttpGet]
+    [Route("details/email")]
+    [AllowAnonymous]
+    [ResponseCache(NoStore = true)]
+    [Produces(HttpContentType.JSON)]
+    [ProducesResponseType(typeof(User), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    public virtual async Task<IActionResult> GetUserByEmailAsync([FromQuery][Required][EmailAddress] string emailAddress, CancellationToken cancellationToken = default)
+    {
+        var user = await this.Repository
+            .GetFirstAsync<User>(x => x.IdentityUser.Email == emailAddress, new Ordering(), cancellationToken);
+
+        if (user == null)
+        {
+            return this.NotFound();
+        }
+
+        return this.Ok(user);
+    }
+
+    /// <summary>
+    /// Get new sign-ups.
+    /// </summary>
+    /// <param name="cancellationToken">The token used when request is cancelled.</param>
+    /// <returns>The users.</returns>
+    /// <response code="200">OK.</response>
+    /// <response code="404">Not Found.</response>
+    /// <response code="400">Bad Request.</response>
+    /// <response code="401">Unauthorized.</response>
+    /// <response code="500">Error occurred.</response>
+    [HttpGet]
+    [Route("new-signups")]
+    [AllowAnonymous]
+    [ResponseCache(NoStore = true)]
+    [Produces(HttpContentType.JSON)]
+    [ProducesResponseType(typeof(IEnumerable<User>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    public virtual async Task<IActionResult> GetNewSignUpsAsync(CancellationToken cancellationToken = default)
+    {
+        var user = await this.Repository
+            .GetManyAsync<User>(x => x.CreatedAt > DateTimeOffset.UtcNow.AddHours(-2), cancellationToken);
+
+        return this.Ok(user);
+    }
+}
---
+++ Svc.Accounts/Data/MySqlProvider.cs
@@ -0,0 +1,98 @@
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
+using Nano.Common.Mvc.HealthChecks.Extensions;
+using Nano.Data.Abstractions;
+using Nano.Data.Abstractions.Config;
+using Nano.Data.Extensions;
+using System;
+
+namespace Nano.Data.MySql;
+
+/// <summary>
+/// MySQL data provider using Pomelo.EntityFrameworkCore.MySql.
+/// </summary>
+/// <remarks>
+///     Supports retry policies, batching, spatial data via NetTopologySuite, query splitting behavior, and optional health checks.
+///     Documentation: https://github.com/Nano-Core/Nano.Library/blob/master/Nano.Data.MySql/README.md#nanodatamysql.
+/// </remarks>
+public sealed class MySqlProvider2 : IDataProvider
+{
+    /// <inheritdoc />
+    public static void Configure(IServiceCollection services, DataOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(options);
+
+        services
+            .AddSingleton<IDatabaseExceptionTranslator, MySqlExceptionTranslator2>();
+
+        if (options.HealthCheck != null)
+        {
+            var failureStatus = options.HealthCheck.UnhealthyStatus
+                .GetHealthStatus();
+
+            services
+                .AddHealthChecks()
+                .AddMySql(options.ConnectionString, name: "mysql", failureStatus: failureStatus);
+        }
+    }
+
+    /// <inheritdoc />
+    public static void Configure(DbContextOptionsBuilder builder, DataOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(options);
+
+        var batchSize = options.BatchSize;
+        var retryCount = options.QueryRetryCount;
+        var connectionString = options.ConnectionString;
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+        if (false)
+        {
+            //const string DEFAULT_URL = "https://ossrdbms-aad.database.windows.net/.default";
+
+            //var credential = new WorkloadIdentityCredential();
+            //var dataSourceBuilder = new MySqlDataSourceBuilder(connectionString);
+
+            //dataSourceBuilder
+            //    .UsePeriodicPasswordProvider(
+            //        async (_, cancellationToken) =>
+            //        {
+            //            var request = new TokenRequestContext([DEFAULT_URL]);
+
+            //            var token = await credential
+            //                .GetTokenAsync(request, cancellationToken);
+
+            //            return token.Token;
+            //        }, TimeSpan.FromMinutes(50), TimeSpan.FromSeconds(10));
+
+            //var dataSource = dataSourceBuilder
+            //    .Build();
+
+            //builder
+            //    .UseMySql(dataSource, serverVersion, ConfigureMySql);
+        }
+        else
+        {
+            builder
+                .UseMySql(connectionString, serverVersion, ConfigureMySql);
+        }
+
+        void ConfigureMySql(MySqlDbContextOptionsBuilder mysqlBulder)
+        {
+            var querySplittingBehavior = options.QuerySplittingBehavior
+                .GetQuerySplittingBehavior();
+
+            mysqlBulder
+                .MaxBatchSize(batchSize)
+                .EnableRetryOnFailure(retryCount)
+                .UseNetTopologySuite()
+                .UseQuerySplittingBehavior(querySplittingBehavior);
+        }
+    }
+}
